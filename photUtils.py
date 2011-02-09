e36cbf8b003b5fcc34fe595830951f1fdae53455
ljones <ljones@9508bedb-3015-0410-995b-e0164f5cd44b>
2011-02-09 22:27:08 +0000

    Added more comments to photUtils.

--- photUtils.py
+++ photUtils.py
@@ -40,6 +40,11 @@ def loadBandpasses(filterlist=('u', 'g', 'r', 'i', 'z', 'y'), dataDir=None, filt
     return bandpassDict
 
 def setupPhiArray_dict(bandpassDict, bandpassKeys):
+    """ Generate 2-dimensional numpy array for Phi values in the bandpassDict.
+
+    You must pass in bandpassKeys so that the ORDER of the phiArray and the order of the magnitudes returned by
+    manyMagCalc can be preserved. You only have to do this ONCE and can then reuse phiArray many times for many
+    manyMagCalculations."""
     # Make a list of the bandpassDict for phiArray - in the ORDER of the bandpassKeys
     bplist = []
     for f in bandpassKeys:
@@ -49,6 +54,13 @@ def setupPhiArray_dict(bandpassDict, bandpassKeys):
     return phiArray, wavelenstep
 
 def manyMagCalc_dict(sedobj, phiArray, wavelenstep, bandpassDict, bandpassKeys):
+    """Return a dictionary of magnitudes for a single Sed object.
+
+    You must pass the sed itself, phiArray and wavelenstep for the manyMagCalc itself, but
+    you must also pass the bandpassDictionary and keys so that the sedobj can be resampled onto
+    the correct wavelength range for the bandpass (i.e. maybe you redshifted sedobj??) and so that the
+    order of the magnitude calculation / dictionary assignment is preserved from the phiArray setup previously.
+    Note that THIS WILL change sedobj by resampling it onto the required wavelength range. """
     # Set up the SED for using manyMagCalc - note that this CHANGES sedobj
     # Have to check that the wavelength range for sedobj matches bandpass - this is why the dictionary is passed in.
     if sedobj.needResample(wavelen=wavelen, wavelen_match=bandpassDict[bandpassKeys[0]].wavelen):
