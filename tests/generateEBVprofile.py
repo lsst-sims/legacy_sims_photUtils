0d81e73ab4cb56bf07499808ee80f03bd3a987b7
danielsf <scott.f.daniel@gmail.com>
2014-09-09 13:27:11 -0700

    did control profile

--- tests/generateEBVprofile.py
+++ tests/generateEBVprofile.py
@@ -2,16 +2,43 @@
 #we can determine how long it is taking
 
 import numpy, os
-from lsst.sims.catalogs.generation.utils import makeStarTestDB, myTestStars
+from lsst.sims.catalogs.generation.utils import makeStarTestDB
+from lsst.sims.catalogs.generation.db import DBObject
 from lsst.sims.catalogs.measures.instance import InstanceCatalog
 from lsst.sims.photUtils import EBVmixin
+from lsst.sims.coordUtils import AstrometryStars
 
-class myCatalogClass(InstanceCatalog,EBVmixin):
+class myCatalogClass(InstanceCatalog,EBVmixin,AstrometryStars):
     column_outputs = ['id','raJ2000','decJ2000','EBV']
+    catalog_type = 'myCatalog'
 
-if os.path.exists('testEBVdatabase.db'):
-    os.unlink('testEBVdatabase.db')
-makeStarTestDB(filename='testEBVdatabase.db',size=10000)
+class myTestStars(DBObject):
+    objid = 'teststars'
+    tableid = 'stars'
+    idColKey = 'id'
+    #Make this implausibly large?  
+    appendint = 1023
+    dbAddress = 'sqlite:///testEBVdatabase.db'
+    raColName = 'ra'
+    decColName = 'decl'
+    columns = [('id', None, int),
+               ('raJ2000', 'ra*%f'%(numpy.pi/180.)),
+               ('decJ2000', 'decl*%f'%(numpy.pi/180.)),
+               ('umag', None),
+               ('gmag', None),
+               ('rmag', None),
+               ('imag', None),
+               ('zmag', None),
+               ('ymag', None),
+               ('magNorm', 'mag_norm', float)]
 
+
+#if os.path.exists('testEBVdatabase.db'):
+#    os.unlink('testEBVdatabase.db')
+#makeStarTestDB(filename='testEBVdatabase.db',size=10000)
+
+myDB=DBObject.from_objid('teststars')
+myCatalog=myDB.getCatalog('myCatalog')
+myCatalog.write_catalog('test_catalog.sav')
 
 
--- tests/profileEBVtime.py
+++ tests/profileEBVtime.py
@@ -1,4 +1,4 @@
 import pstats
 
-p=pstats.Stats('junk.sav')
+p=pstats.Stats('control_profile.sav')
 p.strip_dirs().sort_stats('filename').print_stats('EBV.py')
