bdc2665548c1cecec88c1b163550de3dad7f8c8d
danielsf <scott.f.daniel@gmail.com>
2014-09-10 11:34:00 -0700

    checked 10 instances of 50,0000 objects in control and test; looks like we achieved a factor of 2/3 speed-up

--- tests/profileEBVtime.py
+++ tests/profileEBVtime.py
@@ -1,9 +1,14 @@
 import pstats
 
 print "control\n"
-p=pstats.Stats('control_profile.sav')
-p.strip_dirs().sort_stats('filename').print_stats('EBV.py')
+for i in range(10):
+
+    name = 'control_profile_'+str(i)+'.sav'
+    p=pstats.Stats(name)
+    p.strip_dirs().sort_stats('name').print_stats('get_EBV')
 
 print "test\n"
-pp=pstats.Stats('test_profile.sav')
-pp.strip_dirs().sort_stats('filename').print_stats('EBV.py')
+for i in range(10):
+    name='test_profile_'+str(i)+'.sav'
+    pp=pstats.Stats(name)
+    pp.strip_dirs().sort_stats('name').print_stats('get_EBV')
