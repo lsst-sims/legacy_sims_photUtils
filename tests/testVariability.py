250474561e9a20db9b16241b9fed74150de0f411
danielsf <scott.f.daniel@gmail.com>
2014-10-20 15:40:17 -0700

    added test for rrlyrae stars

--- tests/testVariability.py
+++ tests/testVariability.py
@@ -31,7 +31,7 @@ def makeMflareTable(size=10, **kwargs):
     for i in xrange(size):
         sedFile = sedFiles[numpy.random.randint(0,3)]
         varParam = {'varMethodName':'applyMflare', 
-           'pars':{'t0':48000.0, 'lcfilename':lcFiles[numpy.random.randint(0,3)], 'dt':0.00069444418, 'length': 1825}}
+           'pars':{'t0':48000.0, 'lcfilename':lcFiles[numpy.random.randint(0,len(lcFiles))], 'dt':0.00069444418, 'length': 1825}}
         paramStr = json.dumps(varParam)
         
         qstr = '''INSERT INTO mFlare VALUES (%i, '%s', '%s')''' % (i, paramStr,sedFile)
@@ -39,21 +39,55 @@ def makeMflareTable(size=10, **kwargs):
     conn.commit()
     conn.close()
 
-class mflareDB(CatalogDBObject):
-    objid = 'mflareTest'
-    tableid = 'mFlare'
+def makeRRlyTable(size=100, **kwargs):
+    """
+    Make a test database to serve information to the mflareTest object
+    """
+    sedFiles = ['kp10_8750.fits_g35_8950','kp03_10500.fits_g45_10600','km50_6750.fits_g20_6750']
+    lcFiles = ['rrly_lc/RRc/959802_per.txt','rrly_lc/RRc/1078860_per.txt','rrly_lc/RRab/98874_per.txt',
+               'rrly_lc/RRab/3879827_per.txt']
+
+    conn = sqlite3.connect('VariabilityTestDatabase.db')
+    c = conn.cursor()
+    try:
+        c.execute('''CREATE TABLE RRly
+                     (varsimobjid int, varParamStr text, sedfilename text)''')
+        conn.commit()
+    except:
+        raise RuntimeError("Error creating database.")
+
+    numpy.random.seed(32)
+    mjDisplacement = (numpy.random.sample(size)-50.0)*50.0
+    for i in xrange(size):
+        sedFile = sedFiles[numpy.random.randint(0,3)]
+        varParam = {'varMethodName':'applyRRly', 
+           'pars':{'tStartMjd':48000.0+mjDisplacement[i], 'filename':lcFiles[numpy.random.randint(0,len(lcFiles))]}}
+        paramStr = json.dumps(varParam)
+        
+        qstr = '''INSERT INTO RRly VALUES (%i, '%s', '%s')''' % (i, paramStr,sedFile)
+        c.execute(qstr)
+    conn.commit()
+    conn.close()
+
+class variabilityDB(CatalogDBObject):
     dbAddress = 'sqlite:///VariabilityTestDatabase.db'
     idColKey = 'varsimobjid'
     columns = [('id','varsimobjid',int),
                ('sedFilename','sedfilename',str,40)]
 
-class mflareCatalog(InstanceCatalog,PhotometryStars,Variability):
-    catalog_type = 'mflareCatalog'
-    column_outputs = ['varsimobjid','sedFilename','lsst_u_var']
-    
+class mflareDB(variabilityDB):
+    objid = 'mflareTest'
+    tableid = 'mFlare'
+
+class rrlyDB(variabilityDB):
+    objid = 'rrlyTest'
+    tableid = 'RRly'
+
+class variabilityCatalog(InstanceCatalog,PhotometryStars,Variability):
+    catalog_type = 'variabilityCatalog'
+    column_outputs = ['varsimobjid','sedFilename','lsst_u_var','lsst_u']
     default_columns=[('magNorm',14.0,float)]
     
-
 class VariabilityTest(unittest.TestCase):
 
     @classmethod
@@ -73,13 +107,23 @@ class VariabilityTest(unittest.TestCase):
         del self.obs_metadata
 
     def testMflares(self):
+        print "testing mflares"
         makeMflareTable()
         myDB = CatalogDBObject.from_objid('mflareTest')
-        myCatalog = myDB.getCatalog('mflareCatalog',obs_metadata=self.obs_metadata)
-        myCatalog.write_catalog('mFlareTestCatalog.dat',chunk_size=1)
+        myCatalog = myDB.getCatalog('variabilityCatalog',obs_metadata=self.obs_metadata)
+        myCatalog.write_catalog('mFlareTestCatalog.dat',chunk_size=1000)
 
-        if os.path.exists('mFlareTestCatalog.dat'):
-            os.unlink('mFlareTestCatalog.dat')
+        #if os.path.exists('mFlareTestCatalog.dat'):
+        #    os.unlink('mFlareTestCatalog.dat')
+
+    def testRRlyrae(self):
+        makeRRlyTable()
+        myDB = CatalogDBObject.from_objid('rrlyTest')
+        myCatalog = myDB.getCatalog('variabilityCatalog',obs_metadata=self.obs_metadata)
+        myCatalog.write_catalog('rrlyTestCatalog.dat',chunk_size=1000)
+
+        #if os.path.exists('rrlyTestCatalog.dat'):
+        #    os.unlink('rrlyTestCatalog.dat')
 
 def suite():
     utilsTests.init()
