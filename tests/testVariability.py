208f7c0ac3fd244e25bec4ee28ab41378894f524
danielsf <scott.f.daniel@gmail.com>
2014-10-20 15:52:21 -0700

    wrote test for cepheid variables

--- tests/testVariability.py
+++ tests/testVariability.py
@@ -14,7 +14,7 @@ from lsst.sims.photUtils.Variability import Variability
 
 def makeMflareTable(size=10, **kwargs):
     """
-    Make a test database to serve information to the mflareTest object
+    Make a test database to serve information to the flare test
     """
     sedFiles = ['m2.0Full.dat','m5.1Full.dat', 'm4.9Full.dat']
     lcFiles = ['flare_lc_bin3_4.dat', 'flare_lc_bin1_4.dat', 'flare_lc_bin3_3.dat']
@@ -41,7 +41,7 @@ def makeMflareTable(size=10, **kwargs):
 
 def makeRRlyTable(size=100, **kwargs):
     """
-    Make a test database to serve information to the mflareTest object
+    Make a test database to serve information to the rrlyrae test
     """
     sedFiles = ['kp10_8750.fits_g35_8950','kp03_10500.fits_g45_10600','km50_6750.fits_g20_6750']
     lcFiles = ['rrly_lc/RRc/959802_per.txt','rrly_lc/RRc/1078860_per.txt','rrly_lc/RRab/98874_per.txt',
@@ -69,6 +69,39 @@ def makeRRlyTable(size=100, **kwargs):
     conn.commit()
     conn.close()
 
+def makeCepheidTable(size=100, **kwargs):
+    """
+    Make a test database to serve information to the cepheid test
+    """
+    sedFiles = ['kp10_8750.fits_g35_8950','kp03_10500.fits_g45_10600','km50_6750.fits_g20_6750']
+    lcFiles = ['cepheid_lc/classical_longPer_specfile', 'cepheid_lc/classical_medPer_specfile',
+               'cepheid_lc/classical_shortPer_specfile', 'cepheid_lc/classical_shortPer_specfile',
+               'cepheid_lc/popII_longPer_specfile', 'cepheid_lc/popII_shortPer_specfile']
+
+    conn = sqlite3.connect('VariabilityTestDatabase.db')
+    c = conn.cursor()
+    try:
+        c.execute('''CREATE TABLE cepheid
+                     (varsimobjid int, varParamStr text, sedfilename text)''')
+        conn.commit()
+    except:
+        raise RuntimeError("Error creating database.")
+
+    numpy.random.seed(32)
+    periods = numpy.random.sample(size)*50.0
+    mjDisplacement = (numpy.random.sample(size)-0.5)*50.0
+    for i in xrange(size):
+        sedFile = sedFiles[numpy.random.randint(0,3)]
+        varParam = {'varMethodName':'applyCepheid', 
+           'pars':{'period':periods[i], 'lcfile':lcFiles[i%len(lcFiles)], 't0':48000.0+mjDisplacement[i]}}
+        paramStr = json.dumps(varParam)
+        
+        qstr = '''INSERT INTO cepheid VALUES (%i, '%s', '%s')''' % (i, paramStr,sedFile)
+        c.execute(qstr)
+    conn.commit()
+    conn.close()
+
+
 class variabilityDB(CatalogDBObject):
     dbAddress = 'sqlite:///VariabilityTestDatabase.db'
     idColKey = 'varsimobjid'
@@ -82,6 +115,10 @@ class mflareDB(variabilityDB):
 class rrlyDB(variabilityDB):
     objid = 'rrlyTest'
     tableid = 'RRly'
+
+class cepheidDB(variabilityDB):
+    objid = 'cepheidTest'
+    tableid = 'cepheid'
 
 class variabilityCatalog(InstanceCatalog,PhotometryStars,Variability):
     catalog_type = 'variabilityCatalog'
@@ -105,16 +142,16 @@ class VariabilityTest(unittest.TestCase):
 
     def tearDown(self):
         del self.obs_metadata
-
+    
+    @unittest.skip("until final test; this is still very slow")
     def testMflares(self):
-        print "testing mflares"
         makeMflareTable()
         myDB = CatalogDBObject.from_objid('mflareTest')
         myCatalog = myDB.getCatalog('variabilityCatalog',obs_metadata=self.obs_metadata)
         myCatalog.write_catalog('mFlareTestCatalog.dat',chunk_size=1000)
 
-        #if os.path.exists('mFlareTestCatalog.dat'):
-        #    os.unlink('mFlareTestCatalog.dat')
+        if os.path.exists('mFlareTestCatalog.dat'):
+            os.unlink('mFlareTestCatalog.dat')
 
     def testRRlyrae(self):
         makeRRlyTable()
@@ -122,8 +159,17 @@ class VariabilityTest(unittest.TestCase):
         myCatalog = myDB.getCatalog('variabilityCatalog',obs_metadata=self.obs_metadata)
         myCatalog.write_catalog('rrlyTestCatalog.dat',chunk_size=1000)
 
-        #if os.path.exists('rrlyTestCatalog.dat'):
-        #    os.unlink('rrlyTestCatalog.dat')
+        if os.path.exists('rrlyTestCatalog.dat'):
+            os.unlink('rrlyTestCatalog.dat')
+
+    def testCepheids(self):
+        makeCepheidTable()
+        myDB = CatalogDBObject.from_objid('cepheidTest')
+        myCatalog = myDB.getCatalog('variabilityCatalog',obs_metadata=self.obs_metadata)
+        myCatalog.write_catalog('cepheidTestCatalog.dat',chunk_size=1000)
+
+        #if os.path.exists('cepheidTestCatalog.dat'):
+        #    os.unlink('cepheidTestCatalog.dat')
 
 def suite():
     utilsTests.init()
