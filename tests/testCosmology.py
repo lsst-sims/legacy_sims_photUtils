7706d9c6879f89335bb06281b66841c217eb67c3
danielsf <scott.f.daniel@gmail.com>
2014-10-28 16:18:18 -0700

    added test for non flat w0wa cosomology

--- tests/testCosmology.py
+++ tests/testCosmology.py
@@ -157,6 +157,57 @@ class CosmologyUnitTest(unittest.TestCase):
 
                     del universe
 
+    def testNonFlatW0Wa(self):
+
+        matter = numpy.arange(start=0.15, stop=0.75, step=0.2)
+        darkEnergy = numpy.arange(start=0.1, stop=1.0, step=0.3)
+        ww0 = numpy.arange(start=-1.0, stop = -0.5, step=0.15)
+        wwa = numpy.arange(start = -0.3, stop = 0.3, step=0.15)
+        hubble = numpy.arange(start=50.0, stop=90.0, step=20.0)
+
+        for Om0 in matter:
+            for Ode0 in darkEnergy:
+                for H0 in hubble:
+                    for w0 in ww0:
+                        for wa in wwa:
+
+                            universe = CosmologyWrapper()
+                            universe.Initialize(H0=H0, Om0=Om0, Ode0=Ode0, w0=w0, wa=wa)
+
+                            Og0 = universe.OmegaPhotons(redshift=0.0)
+                            Onu0 = universe.OmegaNeutrinos(redshift=0.0)
+
+                            self.assertTrue(Og0 < 1.0e-4)
+                            self.assertTrue(Onu0 < 1.0e-4)
+                            self.assertAlmostEqual(universe.OmegaMatter(redshift=0.0), Om0, 10)
+                            self.assertAlmostEqual(Ode0, universe.OmegaDarkEnergy(redshift=0.0), 10)
+                            self.assertAlmostEqual(1.0 - Om0 -Ode0 - universe.OmegaCurvature(redshift=0.0),
+                                                   Og0+Onu0, 10)
+                            self.assertAlmostEqual(universe.H(redshift=0.0),H0,10)
+
+                            Om0 = universe.OmegaMatter(redshift=0.0)
+                            Ode0 = universe.OmegaDarkEnergy(redshift=0.0)
+
+                            ztest = numpy.arange(start=0.0, stop=4.0, step=1.0)
+                            for zz in ztest:
+
+                               wControl = w0 + wa*(1.0 - 1.0/(1.0+zz))
+                               self.assertAlmostEqual(wControl, universe.w(redshift=zz), 6)
+
+                               OmControl, OgControl, OnuControl, \
+                               OdeControl, OkControl, Hcontrol = controlOmega(zz, H0, Om0, Og0=Og0, Onu0=Onu0,
+                                                                          w0=w0, wa=wa, Ode0=Ode0)
+
+                               self.assertAlmostEqual(OmControl, universe.OmegaMatter(redshift=zz), 6)
+                               self.assertAlmostEqual(OdeControl, universe.OmegaDarkEnergy(redshift=zz), 6)
+                               self.assertAlmostEqual(OgControl, universe.OmegaPhotons(redshift=zz), 6)
+                               self.assertAlmostEqual(OnuControl, universe.OmegaNeutrinos(redshift=zz), 6)
+                               self.assertAlmostEqual(OkControl, universe.OmegaCurvature(redshift=zz), 6)
+                               self.assertAlmostEqual(Hcontrol, universe.H(redshift=zz), 6)
+
+                            del universe
+
+
 def suite():
     utilsTests.init()
     suites = []
