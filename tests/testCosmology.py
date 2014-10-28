2e0ec2f335c3ddeb158c66d9fd45cc546448ac8e
danielsf <scott.f.daniel@gmail.com>
2014-10-28 15:20:13 -0700

    expanded test to do many calculations in flat LCDM

--- tests/testCosmology.py
+++ tests/testCosmology.py
@@ -6,26 +6,53 @@ from lsst.sims.photUtils import CosmologyWrapper
 
 class CosmologyUnitTest(unittest.TestCase):
 
-    def testParams(self):
-        universe = CosmologyWrapper()
-        Om0=0.24
-        Ode0=0.76
+    def testFlatLCDM(self):
         w0=-1.0
         wa=0.0
-        H0=86.0
-        universe.Initialize(H0=H0, Om0=Om0, Ode0=Ode0, w0=w0, wa=wa)
 
-        Omg = universe.OmegaPhotons(redshift=0.0)
-        Onu = universe.OmegaNeutrinos(redshift=0.0)
+        matter = numpy.arange(start=0.1, stop=0.5, step=0.1)
+        hubble = numpy.arange(start=50.0, stop=90.0, step=5.0)
 
-        self.assertTrue(Omg < 1.0e-4)
-        self.assertTrue(Onu < 1.0e-4)
-        self.assertTrue(numpy.abs(universe.OmegaMatter(redshift=0.0)-Om0) < Omg+Onu)
-        self.assertTrue(numpy.abs(universe.OmegaDarkEnergy(redshift=0.0)-Ode0) < Omg+Onu)
-        self.assertEqual(universe.H(redshift=0.0),H0)
-        self.assertEqual(universe.OmegaCurvature(),0.0)
+        for Om0 in matter:
+            for H0 in hubble:
+                Ode0 = 1.0 - Om0
 
+                universe = CosmologyWrapper()
+                universe.Initialize(H0=H0, Om0=Om0, Ode0=Ode0, w0=w0, wa=wa)
 
+                Og0 = universe.OmegaPhotons(redshift=0.0)
+                Onu0 = universe.OmegaNeutrinos(redshift=0.0)
+
+                self.assertTrue(Og0 < 1.0e-4)
+                self.assertTrue(Onu0 < 1.0e-4)
+                self.assertAlmostEqual(universe.OmegaMatter(redshift=0.0), Om0, 10)
+                self.assertAlmostEqual(Ode0 - universe.OmegaDarkEnergy(redshift=0.0), Og0+Onu0, 6)
+                self.assertAlmostEqual(universe.H(redshift=0.0),H0,10)
+                self.assertEqual(universe.OmegaCurvature(),0.0)
+
+                Om0 = universe.OmegaMatter(redshift=0.0)
+                Ode0 = universe.OmegaDarkEnergy(redshift=0.0)
+
+                ztest = numpy.arange(start=0.0, stop=4.0, step=0.1)
+                for zz in ztest:
+                   aa = (1.0+zz)
+                   Ototal = Om0*numpy.power(aa,3) + (Og0 +Onu0)*numpy.power(aa,4) + Ode0
+                   OmControl = Om0*numpy.power(aa,3)/Ototal
+                   self.assertAlmostEqual(OmControl, universe.OmegaMatter(redshift=zz), 6)
+
+                   OdeControl = Ode0/Ototal
+                   self.assertAlmostEqual(OdeControl, universe.OmegaDarkEnergy(redshift=zz), 6)
+
+                   OgControl = Og0*numpy.power(aa,4)/Ototal
+                   self.assertAlmostEqual(OgControl, universe.OmegaPhotons(redshift=zz), 6)
+
+                   OnuControl = Onu0*numpy.power(aa,4)/Ototal
+                   self.assertAlmostEqual(OnuControl, universe.OmegaNeutrinos(redshift=zz), 6)
+
+                   Hcontrol = H0*numpy.sqrt(Ototal)
+                   self.assertAlmostEqual(Hcontrol, universe.H(redshift=zz), 6)
+
+                del universe
 
 def suite():
     utilsTests.init()
