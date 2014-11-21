1874944358ad687fd01ed7174fa02f494c009e76
danielsf <scott.f.daniel@gmail.com>
2014-11-20 16:40:49 -0800

    pass expmjd to applyVariabilit and calculate_stellar_variability

--- python/lsst/sims/photUtils/Variability.py
+++ python/lsst/sims/photUtils/Variability.py
@@ -42,7 +42,7 @@ class Variability(PhotometryBase):
                                " the lightcurves")
 
     def calculate_stellar_variability(self, u0=None, g0=None, r0=None, i0=None, z0=None, y0=None,
-                                      varParams=None, magNorm=None):
+                                      varParams=None, magNorm=None, expmjd=None):
 
         """
         @param [in] u0 is a numpy array of base u magnitudes
@@ -77,7 +77,7 @@ class Variability(PhotometryBase):
         i=0
         for vv in varParams:
             if vv != numpy.unicode_("None"):
-                deltaMag, deltaMagNorm = self.applyVariability(vv)
+                deltaMag, deltaMagNorm = self.applyVariability(vv, expmjd)
                 uuout.append(u0[i]+deltaMag['u'])
                 ggout.append(g0[i]+deltaMag['g'])
                 rrout.append(r0[i]+deltaMag['r'])
@@ -123,7 +123,8 @@ class Variability(PhotometryBase):
 
         varParams = self.column_by_name('varParamStr')
         return self.calculate_stellar_variability(u0=uu, g0=gg, r0=gg, i0=ii, z0=zz, y0=yy,
-                                                  magNorm=magNorm, varParams=varParams)
+                                                  magNorm=magNorm, varParams=varParams,
+                                                  expmjd=self.obs_metadata.mjd)
 
     @compound('sigma_lsst_u_var','sigma_lsst_g_var','sigma_lsst_r_var',
               'sigma_lsst_i_var','sigma_lsst_z_var','sigma_lsst_y_var')
@@ -211,7 +212,7 @@ class Variability(PhotometryBase):
         i=0
         for vv in varParams:
             if vv != numpy.unicode_("None"):
-                deltaMag, deltaMagNorm=self.applyVariability(vv)
+                deltaMag, deltaMagNorm=self.applyVariability(vv, self.obs_metadata.mjd)
                 uAgnOut.append(uAgn[i]+deltaMag['u'])
                 gAgnOut.append(gAgn[i]+deltaMag['g'])
                 rAgnOut.append(rAgn[i]+deltaMag['r'])
@@ -308,7 +309,7 @@ class Variability(PhotometryBase):
                             agnDict['i'],agnDict['z'],agnDict['y']])
 
 
-    def applyVariability(self, varParams):
+    def applyVariability(self, varParams, expmjd):
         """
         varParams will be the varParamStr column from the data base
 
@@ -335,7 +336,6 @@ class Variability(PhotometryBase):
         varCmd = json.loads(varParams)
         method = varCmd['varMethodName']
         params = varCmd['pars']
-        expmjd=self.obs_metadata.mjd
         output = self._methodRegistry[method](self, params,expmjd)
 
         if self.bandpass is not None:
