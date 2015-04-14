c6cfec831867e73c5026b9e6bddbfefb9e89c7a3
danielsf <scott.f.daniel@gmail.com>
2015-04-14 14:20:01 -0700

    make sure other quantities are floats

--- python/lsst/sims/photUtils/photometricDefaults.py
+++ python/lsst/sims/photUtils/photometricDefaults.py
@@ -10,11 +10,11 @@ class PhotometricDefaults(object):
 
     # The following *wavelen* parameters are default values for gridding wavelen/sb/flambda.
 
-    minwavelen = 300
-    maxwavelen = 1150
+    minwavelen = 300.0
+    maxwavelen = 1150.0
     wavelenstep = 0.1
 
-    lightspeed = 299792458       # speed of light, = 2.9979e8 m/s
+    lightspeed = 299792458.0      # speed of light, = 2.9979e8 m/s
     planck = 6.626068e-27        # planck's constant, = 6.626068e-27 ergs*seconds
     nm2m = 1.00e-9               # nanometers to meters conversion = 1e-9 m/nm
     ergsetc2jansky = 1.00e23     # erg/cm2/s/Hz to Jansky units (fnu)
