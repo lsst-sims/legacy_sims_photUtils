0403b5f75f7805627b7bb64ff4ae733f440b219c
danielsf <scott.f.daniel@gmail.com>
2015-07-16 15:24:52 -0700

    updated testSNR.py

--- tests/testSNR.py
+++ tests/testSNR.py
@@ -5,10 +5,38 @@ import unittest
 import lsst.utils.tests as utilsTests
 
 import lsst.sims.photUtils.SignalToNoise as snr
-from lsst.sims.photUtils import Sed, PhotometryHardware, PhotometricParameters, \
+from lsst.sims.photUtils import Sed, Bandpass, PhotometricParameters, \
                                 LSSTdefaults
 
 class TestSNRmethods(unittest.TestCase):
+
+
+    def setUp(self):
+        hardwareDir = os.path.join(eups.productDir('throughputs'),'baseline')
+        componentList = ['detector.dat', 'm1.dat', 'm2.dat', 'm3.dat',
+                         'lens1.dat', 'lens2.dat', 'lens3.dat']
+        self.skySed = Sed()
+        self.skySed.readSED_flambda(os.path.join(hardwareDir,'darksky.dat'))
+
+        totalNameList = ['total_u.dat', 'total_g.dat', 'total_r.dat', 'total_i.dat',
+                         'total_z.dat', 'total_y.dat']
+
+        self.bpList = []
+        self.hardwareList = []
+        for name in totalNameList:
+            dummy = Bandpass()
+            dummy.readThroughput(os.path.join(hardwareDir, name))
+            self.bpList.append(dummy)
+
+            dummy = Bandpass()
+            hardwareNameList = [os.path.join(hardwareDir, name)]
+            for component in componentList:
+                hardwareNameList.append(os.path.join(hardwareDir, component))
+            dummy.readThroughputList(hardwareNameList)
+            self.hardwareList.append(dummy)
+
+        self.filterNameList = ['u', 'g', 'r', 'i', 'z', 'y']
+
 
     def testMagError(self):
         """
@@ -17,8 +45,6 @@ class TestSNRmethods(unittest.TestCase):
         """
         defaults = LSSTdefaults()
         photParams = PhotometricParameters()
-        phot = PhotometryHardware()
-        phot.loadBandpassesFromFiles()
 
         #create a cartoon spectrum to test on
         spectrum = Sed()
@@ -27,28 +53,28 @@ class TestSNRmethods(unittest.TestCase):
 
         #find the magnitudes of that spectrum in our bandpasses
         magList = []
-        for total in phot.bandpassDict.values():
+        for total in self.bpList:
             magList.append(spectrum.calcMag(total))
         magList = numpy.array(magList)
 
         #try for different normalizations of the skySED
         for fNorm in numpy.arange(1.0, 5.0, 1.0):
-            phot.skySED.multiplyFluxNorm(fNorm)
+            self.skySed.multiplyFluxNorm(fNorm)
             m5List = []
             magSed = []
-            for bp in phot.bandpassDict:
-                total = phot.bandpassDict[bp]
-                hardware = phot.hardwareBandpassDict[bp]
-                seeing = defaults.seeing(bp)
+            for total, hardware, filterName in \
+            zip(self.bpList, self.hardwareList, self.filterNameList):
 
-                m5List.append(snr.calcM5(phot.skySED, total, hardware, photParams,seeing=seeing))
+                seeing = defaults.seeing(filterName)
 
-                magSed.append(snr.calcMagError_sed(spectrum, total, phot.skySED,
+                m5List.append(snr.calcM5(self.skySed, total, hardware, photParams,seeing=seeing))
+
+                magSed.append(snr.calcMagError_sed(spectrum, total, self.skySed,
                                                    hardware, photParams, seeing=seeing))
 
             magSed = numpy.array(magSed)
 
-            magM5 = snr.calcMagError_m5(magList, phot.bandpassDict.values(),
+            magM5 = snr.calcMagError_m5(magList, self.bpList,
                                         numpy.array(m5List), photParams)
 
 
@@ -61,16 +87,14 @@ class TestSNRmethods(unittest.TestCase):
         """
         defaults = LSSTdefaults()
         photParams = PhotometricParameters()
-        phot = PhotometryHardware()
-        phot.loadBandpassesFromFiles()
 
         #create a cartoon spectrum to test on
         spectrum = Sed()
         spectrum.setFlatSED()
         spectrum.multiplyFluxNorm(1.0e-9)
 
-        snr.calcSNR_sed(spectrum, phot.bandpassDict['u'], phot.skySED,
-                        phot.hardwareBandpassDict['u'], photParams, seeing=0.7, verbose=True)
+        snr.calcSNR_sed(spectrum, self.bpList[0], self.skySed,
+                        self.hardwareList[0], photParams, seeing=0.7, verbose=True)
 
 
 def suite():
