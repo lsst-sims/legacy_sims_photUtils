66faf5a31e43e58c7a894b1c590dd87f13b37650
danielsf <scott.f.daniel@gmail.com>
2016-08-16 10:18:46 -0700

    made testReadGalfast.py pep8-compliant

--- tests/testReadGalfast.py
+++ tests/testReadGalfast.py
@@ -1,7 +1,5 @@
 import unittest
 import os
-import shutil
-import numpy as np
 import gzip
 import re
 from astropy.io import fits
@@ -20,7 +18,7 @@ class TestReadGalfast(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
 
-        #Set up Test Spectra Directory
+        # Set up Test Spectra Directory
         cls.testSpecDir = os.path.join(getPackageDir('sims_photUtils'),
                                        'tests/cartoonSedTestData/starSed/')
         cls.testKDir = str(cls.testSpecDir + '/kurucz/')
@@ -44,48 +42,51 @@ class TestReadGalfast(unittest.TestCase):
             os.unlink('exampleOutputFits.txt')
 
         if os.path.exists('example.txt'):
-             os.unlink('example.txt')
+            os.unlink('example.txt')
 
         if os.path.exists('gzipExample.txt.gz'):
             os.unlink('gzipExample.txt.gz')
 
         if os.path.exists('exampleFits.fits'):
-             os.unlink('exampleFits.fits')
+            os.unlink('exampleFits.fits')
 
     def testParseGalfast(self):
 
         """Test Read-in of Galfast Header"""
 
         testRG = readGalfast()
-        #First test that exception is raised when an invalid header label is used
+        # First test that exception is raised when an invalid header label is used
         testInvalidHeader = 'lb[2] header'
         self.assertRaises(RuntimeError, testRG.parseGalfast, testInvalidHeader)
 
-        #Next test that '#' is ignored
+        # Next test that '#' is ignored
         testSymbolHeader = '# lb[2]'
         testSymbolDict = testRG.parseGalfast(testSymbolHeader)
-        self.assertEqual(testSymbolDict, {'l':0, 'b':1})
+        self.assertEqual(testSymbolDict, {'l': 0, 'b': 1})
 
-        #Test that new line marker at end of line is ignored
+        # Test that new line marker at end of line is ignored
         testNewLineHeader = '# lb[2] \n '
         testNewLineDict = testRG.parseGalfast(testNewLineHeader)
-        self.assertEqual(testNewLineDict, {'l':0, 'b':1})
+        self.assertEqual(testNewLineDict, {'l': 0, 'b': 1})
 
-        #Next test that extra spaces are ignored
+        # Next test that extra spaces are ignored
         testSpaceHeader = 'lb[2]     radec[2]'
         testSpaceDict = testRG.parseGalfast(testSpaceHeader)
-        self.assertEqual(testSpaceDict, {'l':0, 'b':1, 'ra':2, 'dec':3})
+        self.assertEqual(testSpaceDict, {'l': 0, 'b': 1, 'ra': 2, 'dec': 3})
 
-        #Test that every header value gets put in the right place using different order than usually come out
+        # Test that every header value gets put in the right place using different order than usually come out
         testFullHeader = '# lb[2] XYZ[3] radec[2] absSDSSr{alias=M1;alias=absmag;band=SDSSr;} ' +\
                          'DM comp FeH vcyl[3] pmlb[3] pmradec[3] Am AmInf SDSSugriz[5]{class=magnitude;' +\
                          'fieldNames=0:SDSSu,1:SDSSg,2:SDSSr,3:SDSSi,4:SDSSz;} ' +\
                          'SDSSugrizPhotoFlags{class=flags;}'
-        actualFullHeaderDict = {'l':0, 'b':1, 'X':2, 'Y':3, 'Z':4, 'ra':5, 'dec':6, 'absSDSSr':7, 'DM':8,
-                                'comp':9, 'FeH':10, 'Vr':11, 'Vphi':12, 'Vz':13, 'pml':14, 'pmb':15,
-                                'vRadlb':16, 'pmra':17, 'pmdec':18, 'vRad':19, 'Am':20, 'AmInf':21,
-                                'SDSSu':22, 'SDSSg':23, 'SDSSr':24, 'SDSSi':25, 'SDSSz':26,
-                                'SDSSPhotoFlags':27}
+        actualFullHeaderDict = {'l': 0, 'b': 1, 'X': 2, 'Y': 3, 'Z': 4,
+                                'ra': 5, 'dec': 6, 'absSDSSr': 7, 'DM': 8,
+                                'comp': 9, 'FeH': 10, 'Vr': 11, 'Vphi': 12,
+                                'Vz': 13, 'pml': 14, 'pmb': 15, 'vRadlb': 16,
+                                'pmra': 17, 'pmdec': 18, 'vRad': 19, 'Am': 20,
+                                'AmInf': 21, 'SDSSu': 22, 'SDSSg': 23,
+                                'SDSSr': 24, 'SDSSi': 25, 'SDSSz': 26,
+                                'SDSSPhotoFlags': 27}
         testFullHeaderDict = testRG.parseGalfast(testFullHeader)
         self.assertEqual(testFullHeaderDict, actualFullHeaderDict)
 
@@ -104,14 +105,14 @@ class TestReadGalfast(unittest.TestCase):
         """Make sure all desired input file types are correctly processed and return correct output files"""
 
         testRG = readGalfast()
-        #First test that it makes sure file exists
+        # First test that it makes sure file exists
         self.assertRaises(RuntimeError, testRG.loadGalfast, ['notarealfile.txt'], ['noOutput.txt'])
 
-        #Next test that if an unknown file format is entered it exits
+        # Next test that if an unknown file format is entered it exits
         self.assertRaises(RuntimeError, testRG.loadGalfast, ['notarealfile.dat'], ['noOutput.txt'])
 
-        #Write example files and then load in and make sure example output files are created
-        #First .txt
+        # Write example files and then load in and make sure example output files are created
+        # First .txt
         exampleIn = open('example.txt', 'w')
         inHeader = '# lb[2] radec[2] XYZ[3] absSDSSr{alias=M1;alias=absmag;band=SDSSr;} DM comp FeH ' +\
                    'vcyl[3] pmlb[3] pmradec[3] Am AmInf SDSSugriz[5]{class=magnitude;fieldNames=0:SDSSu,' +\
@@ -125,7 +126,7 @@ class TestReadGalfast(unittest.TestCase):
         exampleIn.write(inData)
         exampleIn.close()
 
-        #Then gzipped. Also testing multiple lines in catalog.
+        # Then gzipped. Also testing multiple lines in catalog.
         exampleGzipIn = gzip.open('gzipExample.txt.gz', 'w')
         exampleGzipIn.write(inHeader)
         exampleGzipIn.write(testComment)
@@ -133,15 +134,15 @@ class TestReadGalfast(unittest.TestCase):
         exampleGzipIn.write(inData)
         exampleGzipIn.close()
 
-        #Finally a fits file, but first make sure to remove pre-existing file
+        # Finally a fits file, but first make sure to remove pre-existing file
         if os.path.isfile('exampleFits.fits'):
             os.remove('exampleFits.fits')
         columnNames = ['lb', 'XYZ', 'radec', 'absSDSSr', 'DM', 'comp', 'FeH', 'vcyl', 'pmlb', 'pmradec',
                        'Am', 'AmInf', 'SDSSugriz', 'SDSSugrizPhotoFlags']
-        columnArrays = [[[1.79371816, -89.02816704]],  [[7.15, 0.22, -421.87]],
+        columnArrays = [[[1.79371816, -89.02816704]], [[7.15, 0.22, -421.87]],
                         [[11.92064832, -27.62775082]], [[8.126]], [[4.366]], [[0]], [[-0.095]],
-                        [[13.7,  -183.4, -6.2]], [[-20.58, -12.60, 13.02]], [[21.34, -11.26, 13.02]],
-                        [[0.037]], [[0.037]],  [[14.350, 12.949, 12.529, 12.381, 12.358]], [[0]]]
+                        [[13.7, -183.4, -6.2]], [[-20.58, -12.60, 13.02]], [[21.34, -11.26, 13.02]],
+                        [[0.037]], [[0.037]], [[14.350, 12.949, 12.529, 12.381, 12.358]], [[0]]]
         columnFormats = ['2E', '3E', '2E', 'E', 'E', 'E', 'E', '3E', '3E', '3E', 'E', 'E', '5E', 'E']
         cols = fits.ColDefs([fits.Column(name = columnNames[0], format = columnFormats[0],
                                          array = columnArrays[0])])
@@ -160,6 +161,7 @@ class TestReadGalfast(unittest.TestCase):
                         msg='file exampleOutputGzip.txt.gz was not created')
         self.assertTrue(os.path.isfile('exampleOutputFits.txt'),
                         msg='file exampleOutputFits.txt was not created')
+
 
 class MemoryTestClass(lsst.utils.tests.MemoryTestCase):
     pass
